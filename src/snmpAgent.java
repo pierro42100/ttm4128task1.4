1c71d13f9745b27175cc2ac17250e48325711349
pierro42100 <pierrick@dhcp-10-22-2-67.wlan.ntnu.no>
2016-10-04 09:43:51 +0200

    Add text writer
    Add text writer

--- src/snmpAgent.java
+++ src/snmpAgent.java
@@ -4,15 +4,22 @@ import java.util.Timer;
 import java.util.TimerTask;
 import java.io.*;
 
+
+
+
 public class snmpAgent {
 
+	
 	public static void main(String[] args) {
-	      
+	     
+				////////////////////////////////////////////////////////////////////////////////////////
+		
         long temps = 2000;                     // time before repeating the task : 2000 = 2 secondes
         long startTime = 0;                    // time before starting the task (0 : immediat start)
         Timer timer = new Timer();             // timer creation
         TimerTask tache = new TimerTask() {    // timer task creation and specification of what will be done
             @Override
+            
                 public void run() {
                     String line; 
                     String value;
@@ -21,21 +28,32 @@ public class snmpAgent {
                     //String[] cmd = {"bash","-c","ls"};
                     //String[] cmd = {"bash","-c","snmpget -v 2c -c ttm4128 127.0.0.1 sysContact.0"};
                     // Total RAM used: .1.3.6.1.4.1.2021.4.6.0 snmpget -v 2c -c ttm4128 127.0.0.1 UCD-SNMP-MIB::memAvailReal.0
+                    
+                    // First data :
                     String[] cmd = {"bash","-c","snmpget -v 2c -c ttm4128 127.0.0.1 ipInReceives.0"};
-                    System.out.println("Hello, world!\n");
-                    try {
+                    
+                    // Second data :
+                    //String[] cmd = {"bash","-c","snmpget -v 2c -c ttm4128 127.0.0.1 ipInDelivers.0"};
+                    
+                    System.out.println("Hello, world!\n"); //test
+                    try { 
                         Process p = Runtime.getRuntime().exec(cmd);
                         BufferedReader input =
-                            new BufferedReader(new InputStreamReader(p.getInputStream()));
-                        while ((line = input.readLine()) != null) {
+                            new BufferedReader(new InputStreamReader(p.getInputStream())); 
+                        while ((line = input.readLine()) != null) { // Read data
                    
                             System.out.println(line);
-                            //695428 kB, try to get only 695428
-                            value = line.substring(line.lastIndexOf(" ")+1);
+                            value = line.substring(line.lastIndexOf(" ")+1); //Get the value
                             //lastWord = line.substring(line.lastIndexOf(" ")-1);
                             //System.out.println(value);
-                            valueInteger = Integer.valueOf(value);
-                            System.out.println(valueInteger);
+                            valueInteger = Integer.valueOf(value); // convert string into integer
+                            System.out.println(valueInteger); //print the integer value
+                            write(value + "\n"); //EOL
+                            // if > treshold (treshold : 5-10 in 15 minutes)
+                            // --> snmptrap	
+                            // else
+                            // nothing
+                            
                         }
                         input.close();
                     } catch (Exception e) {
@@ -46,5 +64,29 @@ public class snmpAgent {
         timer.scheduleAtFixedRate(tache,startTime,temps);  // beginning of the mechanism
  
         }
+	
+	
+	
+		public static void write(String text) {
+		try {
+			
+			File file = new File("./values.txt");
+
+			// if file doesnt exists, then create it
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+
+			FileWriter fw = new FileWriter(file.getAbsoluteFile(),true); //the true will append the new data
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(text);
+			bw.close();
+
+			System.out.println("Done");
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
